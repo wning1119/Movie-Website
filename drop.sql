7eaa27976844c60098dee393c271f8594c6191a9
Ning <alicewning1119@gmail.com>
2016-11-14 22:00:44 -0800

    commit

---
+++ drop.sql
@@ -0,0 +1,12 @@
+drop table Review;
+drop table Sales;
+drop table MovieRating;
+drop table MovieGenre;
+drop table MovieDirector;
+drop table MovieActor;
+drop table MaxPersonID;
+drop table MaxMovieID;
+drop table Movie;
+drop table Actor;
+drop table Director;
+
